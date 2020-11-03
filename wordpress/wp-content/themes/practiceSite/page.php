39cf9182ae82ff5efa9f01e7222d50b7a5104ec5
Spencer FW <srpfw19@gmail.com>
2020-11-03 10:42:07 -0800

    fixing duplicate functions on index and page files again

--- wordpress/wp-content/themes/practiceSite/page.php
+++ wordpress/wp-content/themes/practiceSite/page.php
@@ -1,10 +1,10 @@
 <?php
 get_header();
-if(have_posts());
-while (have_post()) :
-the_post();
-    the_content();
-endwhile;
+if(have_posts()):
+    while (have_posts()) :
+        the_post();
+        the_content();
+    endwhile;
 endif;
 get_footer();
 ?>
